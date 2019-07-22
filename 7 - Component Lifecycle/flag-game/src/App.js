2cc2d24d20f9da42cde6220a51d56115f83b0fa8
Jakedalus <jacquedalus@gmail.com>
2019-07-22 14:40:58 -0400

    Get api call to work, load into state

--- 7 - Component Lifecycle/flag-game/src/App.js
+++ 7 - Component Lifecycle/flag-game/src/App.js
@@ -6,17 +6,30 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      countries: [''],
+      currentFlag: '',
+      correct: 0,
+      incorrect: 0
+    };
   }
 
   componentDidMount() {
     const url = 'https://restcountries.eu/rest/v2/all';
+
+    fetch(url)
+    .then(res => res.json())
+    .then(countries => {
+      console.log(countries);
+      this.setState({countries})
+    });
   }
 
 
   render() {
     return (
       <div className="App">
-        flags
+
       </div>
     );
   }
