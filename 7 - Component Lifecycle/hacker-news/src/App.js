48e0d097f8a27f0f7032c7fd495d11d15cc08e08
Jakedalus <jacquedalus@gmail.com>
2019-07-22 14:31:44 -0400

    Finish up hacker-news example

--- 7 - Component Lifecycle/hacker-news/src/App.js
+++ 7 - Component Lifecycle/hacker-news/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: []
+    };
+  }
+
+  componentDidMount() {
+    const topStories = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    fetch(topStories)
+      .then(data => data.json())
+      .then(data => data.map(id => {
+        const url = `${storyUrlBase}${id}.json`;
+        return fetch(url).then(d => d.json());
+      }))
+      .then(promises => {
+        console.log(promises);
+        return Promise.all(promises)})
+      .then(stories => {
+        console.log(stories);
+        return this.setState({stories})})
+      .catch(error => console.log(error));
+
+  }
+
+  render() {
+    let views = <div>loading...</div>
+    const {stories} = this.state;
+
+    if (stories && stories.length > 0) {
+      views = stories.map(s => (
+        <p key={s.id}>
+          <a href={s.url}>{s.title}</a> from <strong>{s.by}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      ));
+    }
+
+    return (
+      <div className="App">
+        <h1>Hacker News</h1>
+        {views}
+      </div>
+    );
+  } 
 }
 
 export default App;
