44e7b3a34fea4dc81915a56be377649bdef99032
Jakedalus <jacquedalus@gmail.com>
2019-07-22 15:00:08 -0400

    Get random flag to show up, add Header

---
+++ 7 - Component Lifecycle/flag-game/src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="header">
+    <h1>Guess the Flag</h1>
+  </div>
+);
+
+export default Header;
