430808e88db675e1c273632833a7fe89b72c6725
Jakedalus <jacquedalus@gmail.com>
2019-07-27 14:44:47 -0400

    Wire up TodoForm

--- 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/TodoList.js
+++ 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TodoForm from './TodoForm';
 import Todo from './Todo';
 
 const APIURL = '/api/todos';
@@ -12,6 +13,8 @@ class TodoList extends Component {
     this.state ={
       todos: []
     };
+
+    this.addTodo = this.addTodo.bind(this);
   }
 
   componentWillMount() {
@@ -37,6 +40,33 @@ class TodoList extends Component {
     .then(todos => this.setState({todos}));
   }
 
+  addTodo(val) {
+    fetch(APIURL, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({name: val})
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        if (resp.status >= 400 && resp.status < 500) {
+          return resp.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          });
+        } else {
+          let err = {errorMessage: 'Please try again later, server is not responding.'}
+          throw err;
+        }
+      }
+      return resp.json(); 
+    })
+    .then(newTodo => {
+      this.setState({todos: [...this.state.todos, newTodo]});
+    });
+  }
+
 
   render() {
     const todos = this.state.todos.map(t => (
@@ -48,6 +78,7 @@ class TodoList extends Component {
     return (
       <div>
         <h1>Todo List</h1>
+        <TodoForm addTodo={this.addTodo}/>
         <ul>
           {todos}
         </ul>
