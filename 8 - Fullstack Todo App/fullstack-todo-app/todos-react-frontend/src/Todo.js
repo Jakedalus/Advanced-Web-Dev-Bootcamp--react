45685e143b326c9b5f66ad6c6984429684b3daef
Jakedalus <jacquedalus@gmail.com>
2019-07-27 15:34:59 -0400

    Wire up toggle todo

--- 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/Todo.js
+++ 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/Todo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Todo = ({name, completed, deleteTodo}) => (
+const Todo = ({name, completed, deleteTodo, toggleTodo}) => (
 
-  <li 
-    style={{textDecoration: completed ? 'line-through' : 'none'}}
-  >
-    {name}
+  <li>
+    <span 
+      style={{textDecoration: completed ? 'line-through' : 'none'}}
+      onClick={toggleTodo}
+    >
+      {name}
+    </span>
     <span onClick={deleteTodo}> X </span>
   </li>  
 
--- 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/TodoList.js
+++ 8 - Fullstack Todo App/fullstack-todo-app/todos-react-frontend/src/TodoList.js
@@ -93,6 +93,39 @@ class TodoList extends Component {
     });
   }
 
+  toggleTodo(todo) {
+    const updateURL = APIURL + todo._id;
+
+    fetch(updateURL, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({completed: !todo.completed})
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        if (resp.status >= 400 && resp.status < 500) {
+          return resp.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          });
+        } else {
+          let err = {errorMessage: 'Please try again later, server is not responding.'}
+          throw err;
+        }
+      }
+      return resp.json(); 
+    })
+    .then(updatedTodo => {
+      const todos = this.state.todos.map(todo => 
+        todo._id === updatedTodo._id 
+        ? {...todo, completed: !todo.completed} 
+        : todo
+      );
+      this.setState({todos});
+    });
+  }
 
   render() {
     const todos = this.state.todos.map(t => (
@@ -100,6 +133,7 @@ class TodoList extends Component {
         key={t._id}
         {...t}
         deleteTodo={this.deleteTodo.bind(this, t._id)}
+        toggleTodo={this.toggleTodo.bind(this, t)}
       />
     ));
     return (
