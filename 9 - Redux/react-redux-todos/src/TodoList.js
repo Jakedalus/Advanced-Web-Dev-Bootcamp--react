3a77d3d81932faeb9bcf0ace520380bce2c26fcd
Jakedalus <jacquedalus@gmail.com>
2019-07-31 16:35:55 -0400

    Add adding and removing todo capabilities

--- 9 - Redux/react-redux-todos/src/TodoList.js
+++ 9 - Redux/react-redux-todos/src/TodoList.js
@@ -6,16 +6,68 @@ class TodoList extends Component {
   
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+
+    this.state = {
+      task: ""
+    };
   }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value}); 
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.dispatch({
+      type: 'ADD_TODO',
+      task: this.state.task
+    });
+    this.setState({task: ''});
+  }
+
+  removeTodo(id){
+    this.props.dispatch({
+      type: 'REMOVE_TODO',
+      id
+    });
+  } 
 
   render() {
     return (
-      <ul>
-        { this.state.todos.map((task,idx) => <Todo task={task} key={idx} />) }
-      </ul>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task">Task: </label>
+          <input 
+            type="text" 
+            name="task" 
+            id="task" 
+            value={this.state.task}
+            onChange={this.handleChange}
+          />
+          <button>Add Todo</button>
+        </form>
+        <ul>
+          { this.props.todos.map((todo,idx) => 
+            <Todo 
+              removeTodo={this.removeTodo.bind(this, todo.id)} 
+              task={todo.task} 
+              key={idx} 
+              />
+          )}
+        </ul>
+      </div>
     );
   }
 
 }
 
-export default connect()(TodoList);
+function mapStateToProps(reduxState) {
+  return {
+    todos: reduxState.todos
+  }
+}
+
+export default connect(mapStateToProps)(TodoList);
